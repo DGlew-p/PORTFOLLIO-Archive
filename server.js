8ee6fda032cb6e8ab1ba5e6786ba60f3b21c2a5c
dGlew <darrenjglew@outlook.com>
2022-03-02 18:00:34 -0600

    add dotenv

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
+require("dotenv").config();
 const nodemailer = require("nodemailer");
 const creds = require("./.env");
 const user = { user: creds.USER };
